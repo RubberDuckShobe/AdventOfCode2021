a67cf5ce1b19c9df8c726a9033a02aab27bd16af
RubberDuckShobe <dog@dog.dog>
2021-12-02 16:04:04 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part one
+            int largerMeasurements = 0;
+            string[] input = File.ReadAllLines(@"..\..\..\input.txt");
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (int.Parse(input[i - 1]) < int.Parse(input[i]))
+                {
+                    largerMeasurements++;
+                }
+            }
+            Console.WriteLine("Amount of larger measurements: " + largerMeasurements);
+
+            //Part two
+            int largerSums = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    int sumA = int.Parse(input[i]) + int.Parse(input[i + 1]) + int.Parse(input[i + 2]);
+                    int sumB = int.Parse(input[i + 1]) + int.Parse(input[i + 2]) + int.Parse(input[i + 3]);
+                    if (sumA < sumB)
+                    {
+                        largerSums++;
+                    }
+                }
+                catch (Exception e)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Amount of larger sums: " + largerSums);
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hPos = 0;
+            int depth = 0;
+            int aim = 0;
+            string[] input = File.ReadAllLines(@"..\..\..\input.txt");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].StartsWith("forward"))
+                {
+                    int value = GetValue(input[i]);
+                    hPos += value;
+                    depth += aim * value;
+                }
+                if (input[i].StartsWith("down"))
+                {
+                    aim += GetValue(input[i]);
+                }
+                if (input[i].StartsWith("up"))
+                {
+                    aim -= GetValue(input[i]);
+                }
+            }
+
+            Console.WriteLine("Horizontal position: " + hPos);
+            Console.WriteLine("Depth: " + depth);
+            Console.WriteLine("HPos x Depth = " + depth * hPos);
+        }
+
+        static int GetValue(string input)
+        {
+            int spacePos = input.IndexOf(" ");
+            string valueString = input.Substring(spacePos + 1);
+            return int.Parse(valueString);
+        }
+    }
+}
